6581222ecbe3e22788d55e8d761c1e3a16bcf9b8
Neeraj Singh <nnneerajjj@gmail.com>
2021-08-02 22:23:43 +0530

    login logout

--- Gemfile
+++ Gemfile
@@ -60,3 +60,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass'
 gem 'bcrypt'
+gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get 'static_pages/about', as: 'static_pages_about'
   get 'static_pages/contact', as: 'static_pages_contact'
   get 'signup'  => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
